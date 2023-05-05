b19a0e1064e6bf36d5841cfa4c5c86441c550164
unknown <stevanusfirman22@gmail.com>
2023-05-05 18:31:32 +0700

    Second Commit

--- spotifyweb/src/App.jsx
+++ spotifyweb/src/App.jsx
@@ -1,10 +1,15 @@
-import "./App.css";
 import Login from "./Login.jsx";
+import Osic from "./Osic.jsx";
+import "./App.css";
+import { Route, Link, Routes } from "react-router-dom";
 
 function App() {
   return (
     <div className="App">
-      <Login />
+      <Routes>
+        <Route exact path="/login" Component={Login}></Route>
+        <Route exact path="/osic" Component={Osic}></Route>
+      </Routes>
     </div>
   );
 }
--- spotifyweb/src/Login.jsx
+++ spotifyweb/src/Login.jsx
@@ -1,34 +1,34 @@
 import React from "react";
-import MediaQuery from "react-responsive";
 import "./Login.css";
 
 function Login() {
   return (
-    <MediaQuery minWidth={412}>
-      <div className="Bungkus">
-        <img
-          className="background"
-          src="https://wallpaperaccess.com/full/880901.png"
-          alt="background"
-        />
-        <div className="login">
-          <h2>Sign In</h2>
-          <form className="inputBox">
-            <input className="data" type="text" placeholder="Username" />
-          </form>
-          <form className="inputBox">
-            <input className="data" type="password" placeholder="Password" />
-          </form>
-          <form className="inputBox">
-            <input type="submit" value="Login" className="btn" />
-          </form>
-          <div className="group">
-            <a href="#">Forget Password</a>
-            <a href="3">Sign Up</a>
-          </div>
+    <div className="container">
+      <img
+        className="background"
+        src="https://wallpaperaccess.com/full/880901.png"
+        alt="background"
+      />
+      <div className="login">
+        <h1>
+          Welcome to <a>Our Music</a>
+        </h1>
+        <h2>Sign In</h2>
+        <form className="inputBox">
+          <input className="data" type="text" placeholder="Username" />
+        </form>
+        <form className="inputBox">
+          <input className="data" type="password" placeholder="Password" />
+        </form>
+        <form className="inputBox">
+          <input type="submit" value="Login" className="btn" />
+        </form>
+        <div className="group">
+          <a href="#">Forget Password</a>
+          <a href="3">Sign Up</a>
         </div>
       </div>
-    </MediaQuery>
+    </div>
   );
 }
 
